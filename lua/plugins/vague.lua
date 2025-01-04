65d0aa1f053269889b127db510a342369ee371e3
Aryan <aryan@gupta-inc.com>
2025-01-04 15:55:48 -0800

    live in the terminal

--- lua/plugins/vague.lua
+++ lua/plugins/vague.lua
@@ -1,13 +1,15 @@
 return {
-  {
-    "vague2k/vague.nvim",
-    config = function()
-      require("vague").setup {
-        -- optional configuration here
+  "vague2k/vague.nvim",
+  config = function()
+    require("vague").setup({
+      style = {
+        comments = "none",  -- Override default italic
+        strings = "none",   -- Override default italic
+      },
+      colors = {
+        pmenu = "#1d1e1f",      -- Popup menu background
+        pmenusel = "#2a2b2c",   -- Selected item highlight
       }
-      -- Set VertSplit color to black and remove italics
-      vim.cmd[[highlight VertSplit guifg=black guibg=NONE gui=NONE cterm=NONE]]
-    end,
-  },
+    })
+  end
 }
-
