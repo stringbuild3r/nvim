8dc3582280115de8a951b379937abc856e279c57
Aryan <aryan@gupta-inc.com>
2025-02-03 12:03:18 -0800

    oil tree swap

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,17 @@
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  config = function()
+    require("oil").setup()
+    
+    -- Keymap to open Oil with '-'
+    vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory with Oil" })
+  end,
+}
