cd9e9441341248ecae48a2b5b910f46b7fca54a6
Aryan <aryan@gupta-inc.com>
2025-04-13 21:40:58 -0700

    gruvbox

--- lua/plugins/cyberdream.lua
+++ lua/plugins/cyberdream.lua
@@ -2,4 +2,10 @@ return {
     "scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+        highlights = {
+            Comment = { bold = true }
+        }
+        -- You can add other Cyberdream options here
+    }
 }
